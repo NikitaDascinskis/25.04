18706bdfd2da45308a9106ee55248e8f5ec03887
NikitaNKS <91866260+NikitaDascinskis@users.noreply.github.com>
2022-04-25 05:19:43 +0000

    Initial commit

---
+++ 2uzdevuma.py
@@ -0,0 +1,9 @@
+number = int(input())  // Mainīgā lieluma deklarēšana + ieviešana
+
+if (number % 2 == 0):  // Ja, dalot skaitli ar 2, atlikums = 0
+
+   print("pāra")  // Tas ir pāri
+
+else:  // Citādi
+
+   print("nepāra") // Tas ir nepāra
